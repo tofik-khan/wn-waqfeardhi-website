f129275963eae30679e452a319e570064c994e6d
Tofik Khan <tofik.khan@mkausa.org>
2023-02-02 17:28:34 -0800

    Create Chip component

---
+++ components/Chip.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+
+`
+
+const StyledIcon = styled.img`
+`
+
+const StyledText = styled.span`
+`
+
+export default function Chip() {
+    return <>Chip</>
+}
