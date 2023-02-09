c36a1321feff69c255df39edf6e557fecaa476b4
Tofik Khan <tofik.khan@mkausa.org>
2023-02-08 23:11:51 -0800

    Add GoogleAuth via GoogleAPI

--- pages/api/get-listings.js
+++ pages/api/get-listings.js
@@ -1,3 +1,13 @@
 export default async function handler (req, res) {
+
+    const express = require("express");
+    const { google } = require("googleapis");
+
+    const auth = new google.auth.GoogleAuth({
+        keyFile: `${process.env.GOOGLE_API_KEY_FILE}`, //the key file
+        //url to spreadsheets API
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+      });
+
     res.status(200).json({success: true});
 }
