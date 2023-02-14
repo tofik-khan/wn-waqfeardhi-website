30efcd572f5e154da895e7a6076bccb086ad5dc2
Tofik Khan <tofik.khan@mkausa.org>
2023-02-14 12:09:26 -0800

    Add form state and update logic. Need to add onChange to Button

--- pages/listings/[slug].js
+++ pages/listings/[slug].js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
@@ -7,6 +8,7 @@ import Col from "react-bootstrap/Col"
 import { getData, TITLE, SUBTITLE, DESCRIPTION, SLUG, TAGS } from "/pages/api/get-listings-data";
 
 import TextInput from "/components/TextInput";
+import Button from "/components/Button";
 
 export default function Page () {
   const router = useRouter()
@@ -15,10 +17,13 @@ export default function Page () {
   const listings = getData();
 
   const selectedListing = listings.filter((element) => element[SLUG] === slug)[0];
-  console.log(selectedListing);
   if(selectedListing.length === 0) {
     return <h1>Listing not Found</h1>
   }
+
+  const [formData, updateFormData] = useState({});
+  console.log(formData)
+
   return (
     <>
         <h1 className="py-5 text-center">{selectedListing[TITLE]}</h1>
@@ -33,12 +38,15 @@ export default function Page () {
                 <h2>Submit Form</h2>
             </Row>
             <Row>
-                <Col sm={6}><TextInput label="First Name" /></Col>
-                <Col sm={6}><TextInput label="Last Name" /></Col>
+                <Col sm={6}><TextInput label="First Name" onChange={(event) => updateFormData({...formData, firstname: event.target.value})}/></Col>
+                <Col sm={6}><TextInput label="Last Name"  onChange={(event) => updateFormData({...formData, lastname: event.target.value})}/></Col>
             </Row>
             <Row className='py-2'>
-                <Col sm={6}><TextInput label="Email" /></Col>
-                <Col sm={6}><TextInput label="Phone Number" /></Col>
+                <Col sm={6}><TextInput label="Email" onChange={(event) => updateFormData({...formData, email: event.target.value})}/></Col>
+                <Col sm={6}><TextInput label="Phone Number" onChange={(event) => updateFormData({...formData, phone: event.target.value})}/></Col>
+            </Row>
+            <Row>
+                <Col><Button variant="primary" onClick={() => console.log(formData)}>Submit</Button></Col>
             </Row>
         </Container>
     </>
