d96434391187636fa5fd2be6b36b6a2ec6123ccd
Tofik Khan <tofik.khan@mkausa.org>
2023-02-14 18:57:02 -0800

    Add Submitted Message

--- pages/listings/[slug].js
+++ pages/listings/[slug].js
@@ -16,6 +16,7 @@ export default function Page () {
 
   const [listings, updateListings] = useState([]);
   const [formData, updateFormData] = useState({});
+  const [screen, updateScreen] = useState("FORM");
 
   useEffect(() => {
     updateListings(getData());
@@ -30,33 +31,44 @@ export default function Page () {
     return <h1>Listing not Found</h1>
   }
 
-  return (
-    <>
-        <h1 className="py-5 text-center">{selectedListing[TITLE]}</h1>
-        <p className='text-center'>{selectedListing[SUBTITLE]}</p>
-        <Container>
-            <Row className="justify-content-center">
-                <Col md={10}>{selectedListing[DESCRIPTION]}</Col>
-            </Row>
-        </Container>
-        <Container className='py-5'>
-            <Row>
-                <h2>Submit Form</h2>
-            </Row>
-            <Row>
-                <Col sm={6}><TextInput label="First Name" onChange={(event) => updateFormData({...formData, firstname: event.target.value})}/></Col>
-                <Col sm={6}><TextInput label="Last Name"  onChange={(event) => updateFormData({...formData, lastname: event.target.value})}/></Col>
-            </Row>
-            <Row className='py-2'>
-                <Col sm={6}><TextInput label="Email" onChange={(event) => updateFormData({...formData, email: event.target.value})}/></Col>
-                <Col sm={6}><TextInput label="Phone Number" onChange={(event) => updateFormData({...formData, phone: event.target.value})}/></Col>
-            </Row>
-            <Row>
-                <Col><Button variant="primary" onClick={() => submitForm(formData, slug)}>Submit</Button></Col>
-            </Row>
-        </Container>
-    </>
-  );
+
+  if(screen === "FORM") {
+    return (
+        <>
+            <h1 className="py-5 text-center">{selectedListing[TITLE]}</h1>
+            <p className='text-center'>{selectedListing[SUBTITLE]}</p>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col md={10}>{selectedListing[DESCRIPTION]}</Col>
+                </Row>
+            </Container>
+            <Container className='py-5'>
+                <Row>
+                    <h2>Submit Form</h2>
+                </Row>
+                <Row>
+                    <Col sm={6}><TextInput label="First Name" onChange={(event) => updateFormData({...formData, firstname: event.target.value})}/></Col>
+                    <Col sm={6}><TextInput label="Last Name"  onChange={(event) => updateFormData({...formData, lastname: event.target.value})}/></Col>
+                </Row>
+                <Row className='py-2'>
+                    <Col sm={6}><TextInput label="Email" onChange={(event) => updateFormData({...formData, email: event.target.value})}/></Col>
+                    <Col sm={6}><TextInput label="Phone Number" onChange={(event) => updateFormData({...formData, phone: event.target.value})}/></Col>
+                </Row>
+                <Row>
+                    <Col><Button variant="primary" onClick={() => {submitForm(formData, slug); updateScreen("SUBMITTED")}}>Submit</Button></Col>
+                </Row>
+            </Container>
+        </>
+    );
+  }
+
+  else if (screen === "SUBMITTED") {
+    return (
+        <>
+            <h1>Submitted</h1>
+        </>
+    )
+  }
 }
 
 async function submitForm(formData, slug) {
@@ -64,7 +76,6 @@ async function submitForm(formData, slug) {
         auth: process.env.API_AUTH_TOKEN,
         formData: {...formData, slug: slug}
     }
-    console.log(body)
 
     const requestOptions = {
         method: "POST",
