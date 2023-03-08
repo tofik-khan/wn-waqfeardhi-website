c6ff86ad0297044d82151f4623d6147163ecf0f1
Tofik Khan <tofik.khan@mkausa.org>
2023-03-07 16:07:02 -0800

    Add spacing after Nav for content

--- partials/Nav.js
+++ partials/Nav.js
@@ -11,36 +11,43 @@ const StyledNavBar = styled(Navbar)`
   padding-bottom: 10px;
 `;
 
+const StyledSpacer = styled.div`
+  margin-bottom: 100px;
+`;
+
 export const Navigation = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <StyledNavBar fixed="top" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">
-          <Image src="/images/wn-logomark.png" width={250} height={48} />
-        </Navbar.Brand>
-        <Navbar.Toggle>
-          <Hamburger toggled={isOpen} toggle={setOpen} />
-        </Navbar.Toggle>
-        <Navbar.Collapse className="justify-content-end align-items-center">
-          <Nav>
-            <Nav.Link href="/listings" className="d-flex align-items-center">
-              Projects
-            </Nav.Link>
-            <Nav.Link
-              href="https://docs.google.com/forms/d/e/1FAIpQLSeHrX5G_aHNlpF8TQqBxU8L19CmHV9s2K_OKZUMm-jnJjz04A/viewform?pli=1"
-              target="_blank"
-              rel="noreferrer"
-              className="align-items-center"
-            >
-              <Button variant="primary" size="small">
-                Sponsor
-              </Button>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </StyledNavBar>
+    <>
+      <StyledNavBar fixed="top" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <Image src="/images/wn-logomark.png" width={250} height={48} />
+          </Navbar.Brand>
+          <Navbar.Toggle>
+            <Hamburger toggled={isOpen} toggle={setOpen} />
+          </Navbar.Toggle>
+          <Navbar.Collapse className="justify-content-end align-items-center">
+            <Nav>
+              <Nav.Link href="/listings" className="d-flex align-items-center">
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                href="https://docs.google.com/forms/d/e/1FAIpQLSeHrX5G_aHNlpF8TQqBxU8L19CmHV9s2K_OKZUMm-jnJjz04A/viewform?pli=1"
+                target="_blank"
+                rel="noreferrer"
+                className="align-items-center"
+              >
+                <Button variant="primary" size="small">
+                  Sponsor
+                </Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </StyledNavBar>
+      <StyledSpacer />
+    </>
   );
 };
