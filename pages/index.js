ade1ffaa3a71eb31597b982031dc5721d171c1eb
Tofik Khan <tofik.khan@mkausa.org>
2023-03-07 22:03:57 -0800

    Create 'Start Waqf' banner on homepage

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,40 @@ import { Navigation } from "../partials/Nav";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Banner from "../components/Banner";
+import { Heading2, Paragraph } from "../components/Text";
+import Button from "../components/Button";
 
 export default function Page() {
   return (
     <>
       <Navigation />
-      HomePage
+      <Banner variant="light">
+        <Container>
+          <Row>
+            <Col>
+              <Heading2 align={"center"}>Start your Waqf</Heading2>
+            </Col>
+          </Row>
+          <Row className="justify-content-center py-3">
+            <Col md={8}>
+              <Paragraph align={"center"}>
+                Joining Waqf-e-Arzi can be a transformative experience that will
+                enhance all aspects of your life. Take the first step towards
+                spiritual growth and personal development by joining Waqf-e-Arzi
+                today.
+              </Paragraph>
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col xs={"auto"}>
+              <Button variant="primary" size="large" href="/listings">
+                Browse Projects
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </Banner>
     </>
   );
 }
