66de902d29d9cfbd7e37bedf7dcb8decf8210fe5
Tofik Khan <tofik.khan@mkausa.org>
2023-03-09 23:28:47 -0800

    Update data keys to correspond to fetched keys

--- pages/api/get-listings-data.js
+++ pages/api/get-listings-data.js
@@ -7,7 +7,10 @@ export const getData =  () => {
 
 export const TITLE = 0;
 export const SUBTITLE = 1;
-export const DESCRIPTION = 2;
-export const TAGS = 3;
-export const SLUG = 4;
+export const DURATION = 2;
+export const AUDIENCE = 3;
+export const DESCRIPTION = 4;
+export const SLUG = 5;
+export const IMAGE = 6;
+export const PUBLISHED = 7;
 
