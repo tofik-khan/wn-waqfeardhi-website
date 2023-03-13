a2fc831db59bd396cac1252c64b9bf5d72b6caec
Tofik Khan <tofik.khan@mkausa.org>
2023-03-12 17:49:29 -0700

    Update column size for oppy cards for better formatting

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Page() {
         </Row>
         <Row>
           {opportunities.map((element, index) => (
-            <Col xs={6} md={4} lg={3} className="mt-5" key={index}>
+            <Col md={4} lg={3} className="mt-5" key={index}>
               <OpportunityCard
                 image={element.image}
                 heading={element.heading}
