3121d4498dc890ab2d1e86ff83580650b612c764
Tofik Khan <tofik.khan@mkausa.org>
2023-03-23 19:21:24 -0700

    Format document, add default value to dropdown states

--- pages/listings/[slug].js
+++ pages/listings/[slug].js
@@ -17,23 +17,6 @@ export default function Page () {
   const router = useRouter();
   const { slug } = router.query;
 
-  const [listings, updateListings] = useState([]);
-  const [formData, updateFormData] = useState({});
-  const [screen, updateScreen] = useState("FORM");
-
-  useEffect(() => {
-    updateListings(getData());
-  }, [])
-
-  if(listings.length === 0) {
-    return <h1>Loading</h1>
-  }
-
-  const selectedListing = listings.filter((element) => element[SLUG] === slug)[0];
-  if(selectedListing.length === 0) {
-    return <h1>Listing not Found</h1>
-  }
-
   const jammatOptions = require("/content/jammat.json");
   const auxiliaryOptions = [
     { value: "Ansar", label: "Ansarullah" },
@@ -41,75 +24,141 @@ export default function Page () {
     { value: "Khuddam", label: "Khuddam-ul-Ahmadiyya" },
     { value: "Lajna", label: "Lajna Ima'illah" },
     { value: "Nasirat", label: "Nasirat-ul-Ahmadiyya" },
-  ]
+  ];
 
+  const [listings, updateListings] = useState([]);
+  const [formData, updateFormData] = useState({
+    jammat: jammatOptions[0].value,
+    aux: auxiliaryOptions[0].value,
+  });
+  const [screen, updateScreen] = useState("FORM");
 
-  if(screen === "FORM") {
-    return (
-        <>
-            <h1 className="py-5 text-center">{selectedListing[TITLE]}</h1>
-            <p className='text-center'>{selectedListing[SUBTITLE]}</p>
-            <Container>
-                <Row className="justify-content-center">
-                    <Col md={10}>{selectedListing[DESCRIPTION]}</Col>
-                </Row>
-            </Container>
-            <Container className='py-5'>
-                <Row>
-                    <h2>Submit Form</h2>
-                </Row>
-                <Row className='py-2'>
-                    <Col sm={6}>
-                        <TextInput 
-                            label="First Name" 
-                            isError={checkFirstnameError(formData)} 
-                            errorMessage="This field cannot be left blank" 
-                            onChange={(event) => updateFormData({...formData, firstname: event.target.value})}
-                        />
-                    </Col>
-                    <Col sm={6}>
-                        <TextInput 
-                            label="Last Name"
-                            isError={checkLastnameError(formData)} 
-                            errorMessage="This field cannot be left blank" 
-                            onChange={(event) => updateFormData({...formData, lastname: event.target.value})}
-                        />
-                    </Col>
-                </Row>
-                <Row className='py-2'>
-                    <Col sm={6}>
-                        <TextInput 
-                            label="Email"
-                            isError={false} //Todo: Figure out how to determine if email is valid on submit?
-                            errorMessage="This must be a valid email"
-                            onChange={(event) => updateFormData({...formData, email: event.target.value})}
-                        />
-                    </Col>
-                    <Col sm={6}><Dropdown label="Jammat" options={jammatOptions} onChange={(option) => updateFormData({...formData, jammat: option.value})} /></Col>
-                </Row>
-                <Row className='py-2 align-items-center'>
-                    <Col sm={6}><Dropdown label="Auxiliary" options={auxiliaryOptions} onChange={(option) => updateFormData({...formData, aux: option.value})} /></Col>
-                    <Col sm={6} className="pt-4"><ToggleSwitch label="Are you part of the Waqf-e-Nau Scheme?" onChange={(event) => updateFormData({...formData, isWaqfeNau: event.target.checked})} /></Col>
-                </Row>
-                <Row className='py-2 justify-content-center'>
-                    <Col sm={10}>
-                        <TextArea label="Share any relevant experience or comments" onChange={(event) => updateFormData({...formData, comment: event.target.value})} />
-                    </Col>
-                </Row>
-                <Row className='py-2'>
-                    <Col><Button variant="primary" onClick={() => {submitForm(formData, slug); updateScreen("SUBMITTED")}}>Submit</Button></Col>
-                </Row>
-            </Container>
-        </>
-    );
+  useEffect(() => {
+    updateListings(getData());
+  }, []);
+
+  if (listings.length === 0) {
+    return <h1>Loading</h1>;
   }
 
-  else if (screen === "SUBMITTED") {
+  const selectedListing = listings.filter(
+    (element) => element[SLUG] === slug
+  )[0];
+  if (selectedListing.length === 0) {
+    return <h1>Listing not Found</h1>;
+  }
+
+  if (screen === "FORM") {
     return (
-        <>
-            <h1>Submitted</h1>
-        </>
-    )
+      <>
+        <h1 className="py-5 text-center">{selectedListing[TITLE]}</h1>
+        <p className="text-center">{selectedListing[SUBTITLE]}</p>
+        <Container>
+          <Row className="justify-content-center">
+            <Col md={10}>{selectedListing[DESCRIPTION]}</Col>
+          </Row>
+        </Container>
+        <Container className="py-5">
+          <Row>
+            <h2>Submit Form</h2>
+          </Row>
+          <Row className="py-2">
+            <Col sm={6}>
+              <TextInput
+                label="First Name"
+                isError={checkFirstnameError(formData)}
+                errorMessage="This field cannot be left blank"
+                onChange={(event) =>
+                  updateFormData({ ...formData, firstname: event.target.value })
+                }
+              />
+            </Col>
+            <Col sm={6}>
+              <TextInput
+                label="Last Name"
+                isError={checkLastnameError(formData)}
+                errorMessage="This field cannot be left blank"
+                onChange={(event) =>
+                  updateFormData({ ...formData, lastname: event.target.value })
+                }
+              />
+            </Col>
+          </Row>
+          <Row className="py-2">
+            <Col sm={6}>
+              <TextInput
+                label="Email"
+                isError={false} //Todo: Figure out how to determine if email is valid on submit?
+                errorMessage="This must be a valid email"
+                onChange={(event) =>
+                  updateFormData({ ...formData, email: event.target.value })
+                }
+              />
+            </Col>
+            <Col sm={6}>
+              <Dropdown
+                label="Jammat"
+                options={jammatOptions}
+                onChange={(option) =>
+                  updateFormData({ ...formData, jammat: option.value })
+                }
+              />
+            </Col>
+          </Row>
+          <Row className="py-2 align-items-center">
+            <Col sm={6}>
+              <Dropdown
+                label="Auxiliary"
+                options={auxiliaryOptions}
+                onChange={(option) =>
+                  updateFormData({ ...formData, aux: option.value })
+                }
+              />
+            </Col>
+            <Col sm={6} className="pt-4">
+              <ToggleSwitch
+                label="Are you part of the Waqf-e-Nau Scheme?"
+                onChange={(event) =>
+                  updateFormData({
+                    ...formData,
+                    isWaqfeNau: event.target.checked,
+                  })
+                }
+              />
+            </Col>
+          </Row>
+          <Row className="py-2 justify-content-center">
+            <Col sm={10}>
+              <TextArea
+                label="Share any relevant experience or comments"
+                onChange={(event) =>
+                  updateFormData({ ...formData, comment: event.target.value })
+                }
+              />
+            </Col>
+          </Row>
+          <Row className="py-2">
+            <Col>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  submitForm(formData, slug);
+                  updateScreen("SUBMITTED");
+                }}
+              >
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  } else if (screen === "SUBMITTED") {
+    return (
+      <>
+        <h1>Submitted</h1>
+      </>
+    );
   }
 }
 
