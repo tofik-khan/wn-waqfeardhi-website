85cade1471e08bd35089978948b712c9533e31f7
Tofik Khan <tofik.khan@mkausa.org>
2023-07-09 22:21:39 -0700

    Add timestamp to form submissions

--- pages/api/sheets-post.js
+++ pages/api/sheets-post.js
@@ -8,54 +8,56 @@ export default async function handler (req, res) {
     }
 
     const formData = req.body.formData;
+    const timestamp = new Date().toLocaleString();
     const { google } = require("googleapis");
 
     const auth = new google.auth.GoogleAuth({
-        keyFile: `${process.env.GOOGLE_API_KEY_FILE}`, //the key file
-        //url to spreadsheets API
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-      });
+      keyFile: `${process.env.GOOGLE_API_KEY_FILE}`, //the key file
+      //url to spreadsheets API
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
 
     //Auth client Object
     const authClientObject = await auth.getClient();
 
     //Google sheets instance
     const googleSheetsInstance = google.sheets({
-        version: "v4",
-        auth: authClientObject,
+      version: "v4",
+      auth: authClientObject,
     });
 
     // spreadsheet id
     const spreadsheetId = `${process.env.GOOGLE_SHEETS_ID}`;
 
     try {
-        const result = await googleSheetsInstance.spreadsheets.values.append({
-          spreadsheetId,
+      const result = await googleSheetsInstance.spreadsheets.values.append({
+        spreadsheetId,
+        range: "Responses",
+        valueInputOption: "RAW",
+        resource: {
           range: "Responses",
-          valueInputOption: "RAW",
-          resource: {
-            range: "Responses",
-            majorDimension: "ROWS",
-            values: [
-              [
-                formData.firstname,
-                formData.lastname,
-                formData.email,
-                formData.jammat,
-                formData.aux,
-                formData.isWaqfeNau,
-                formData.comment,
-                formData.phone,
-                formData.slug,
-              ],
+          majorDimension: "ROWS",
+          values: [
+            [
+              formData.firstname,
+              formData.lastname,
+              formData.email,
+              formData.jammat,
+              formData.aux,
+              formData.isWaqfeNau,
+              formData.comment,
+              formData.phone,
+              formData.slug,
+              timestamp,
             ],
-          },
-        });
-          console.log(`${result.data.updates.updatedCells} cells appended.`);
-        } catch (err) {
-          console.log("Error: Failed to append to Sheet")
-          console.log(err.message)
-        }
+          ],
+        },
+      });
+      console.log(`${result.data.updates.updatedCells} cells appended.`);
+    } catch (err) {
+      console.log("Error: Failed to append to Sheet");
+      console.log(err.message);
+    }
     
     res.status(200).json({success: true});
 
